1138d228d1971669f062020564febf0f4ef7d2d6
Heshy Roskes <jroskes1@jhu.edu>
2019-08-27 21:02:50 +0200

    mela works up to initialqq

--- fixjetid.py
+++ fixjetid.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
   p.add_argument("outfile")
   p.add_argument("--no-id", action="store_false", dest="applyid")
   p.add_argument("--no-pu-id", action="store_false", dest="applypuid")
+  p.add_argument("--test-no-mela", action="store_true")
   args = p.parse_args()
 
 
@@ -18,8 +19,9 @@ class MelaWrapper(object):
     assert not kwargs
   @property
   def mela(self):
-    from ZZMatrixElement.MELA.mela import Mela
     if self.__mela is None:
+      assert False
+      from ZZMatrixElement.MELA.mela import Mela
       self.__mela = Mela(*self.__melaargs)
     return self.__mela
   def __getattr__(self, attr): return getattr(self.mela, attr)
@@ -35,12 +37,16 @@ class MelaWrapper(object):
 
   def resetInputEvent(self):
     self.__melaeventdummybranch = None
-    self.__mela.resetInputEvent()
+    if self.__mela is not None:
+      self.__mela.resetInputEvent()
 
-from ZZMatrixElement.MELA.mela import Mela, TVar
-mela = MelaWrapper(mela=Mela())
+if args.test_no_mela:
+  mela = MelaWrapper()
+else:
+  from ZZMatrixElement.MELA.mela import Mela
+  mela = MelaWrapper(mela=Mela())
 
-import abc, os
+import abc, os, re
 from itertools import izip
 
 import numpy as np
@@ -189,15 +195,18 @@ def maketlv(pt, eta, phi, m):
   return result
 
 class FirstNJetMomenta(DummyBranch):
-  def __init__(self, n, ptbranch, etabranch, phibranch, massbranch):
+  def __init__(self, n, ptbranch, etabranch, phibranch, massbranch, rejectiftoomany=False):
     super(FirstNJetMomenta, self).__init__("first{}jets")
     self.__n = n
     self.__ptbranch = ptbranch
     self.__etabranch = etabranch
     self.__phibranch = phibranch
     self.__massbranch = massbranch
+    self.__rejectiftoomany = rejectiftoomany
   def setthingfortree(self, t, applyid, applypuid):
-    pt = [_ for _ in self.__ptbranch.lastsetbranchvalue[:self.__n] if _>30]
+    pt = [_ for _ in self.__ptbranch.lastsetbranchvalue if _>30]
+    if self.__rejectiftoomany and len(pt) > self.__n: pt = []
+    pt = pt[:self.__n]
     eta = self.__etabranch.lastsetbranchvalue[:self.__n]
     phi = self.__phibranch.lastsetbranchvalue[:self.__n]
     mass = self.__massbranch.lastsetbranchvalue[:self.__n]
@@ -219,6 +228,7 @@ class MELAEventDummyBranch(DummyBranch):
     super(MELAEventDummyBranch, self).__init__(firstjetmomentabranch.name+"MELA")
     self.__firstjetmomentabranch = firstjetmomentabranch
   def setthingfortree(self, t, applyid, applypuid):
+    if args.test_no_mela: assert False
     from ZZMatrixElement.MELA.mela import SimpleParticleCollection_t
     leptons = SimpleParticleCollection_t(
       (lepid, maketlv(pt, eta, phi, 0))
@@ -234,7 +244,7 @@ class MELAEventDummyBranch(DummyBranch):
   def firstjetmomentabranch(self): return self.__firstjetmomentabranch
 
 class MELAProbability(FirstJetsVariable):
-  def __init__(self, name, melaeventdummybranch, setprocessargnames, couplings, melafunctionname):
+  def __init__(self, name, melaeventdummybranch, setprocessargnames, couplings, melafunctionname, subtractbranches=[]):
     setprocessargs = []
     def function(*jets):
       mela.setInputEvent(melaeventdummybranch)
@@ -244,11 +254,11 @@ class MELAProbability(FirstJetsVariable):
       mela.setProcess(*setprocessargs)
       for coupling, value in couplings.iteritems():
         setattr(mela, coupling, value)
-      return getattr(mela, melafunctionname)()
+      return getattr(mela, melafunctionname)() - sum(_.lastsetbranchvalue for _ in subtractbranches)
     super(MELAProbability, self).__init__(name, function, np.float32, melaeventdummybranch.firstjetmomentabranch, -1)
 
   def compareforxcheck(self, new, old):
-    return np.isclose(new, old, atol=0, rtol=1e-5)
+    return np.isclose(new, old, atol=0, rtol=1e-3)
 
 branches = [
   NJetsBranch("nCleanedJets", lambda pt, sigma, isbtagged, isbtaggedsf, isbtaggedsfup, isbtaggedsfdn: True),
@@ -295,24 +305,72 @@ branches += [
 ]
 
 first2jetmomenta = FirstNJetMomenta(2, jetpt, jeteta, jetphi, jetmass)
+onlyjetmomentum = FirstNJetMomenta(1, jetpt, jeteta, jetphi, jetmass, rejectiftoomany=True)
 dijetmass = FirstJetsVariable("DiJetMass", lambda jet1, jet2: (jet1+jet2).M(), np.float32, first2jetmomenta, -99)
 dijetdeta = FirstJetsVariable("DiJetDEta", lambda jet1, jet2: jet1.Eta() - jet2.Eta(), np.float32, first2jetmomenta, -99)
 
 branches += [
   first2jetmomenta,
+  onlyjetmomentum,
   dijetmass,
   dijetdeta,
   NotRecalculatedBranch("DiJetFisher", -99, np.float32),
 ]
 
 MELAJECnominal2jets = MELAEventDummyBranch(first2jetmomenta)
+MELAJECnominal1jet = MELAEventDummyBranch(onlyjetmomentum)
 
 branches += [
+  MELAJECnominal1jet,
   MELAJECnominal2jets,
-  MELAProbability("p_JJVBF_SIG_ghv1_1_JHUGen_JECNominal", MELAJECnominal2jets, ("SelfDefine_spin0", "JHUGen", "JJVBF"), {"ghz1": 1}, "computeProdP"),
 ]
 
-def fixjetid(infile, outfile, applyid=True, applypuid=True, folders=["ZZTree"]):
+MELAbranchesdict = {}
+
+with open(os.path.join(os.environ["CMSSW_BASE"], "src", "ZZAnalysis", "AnalysisStep", "test", "prod", "pyFragments", "RecoProbabilities.py")) as f:
+  for line in f:
+    line = line.split("#")[0]
+    if "Name:" not in line: continue
+    options = dict(re.findall(r"(\w+):([^ ]+)", line))
+    if "BestDJJ" in options["Name"]:
+      branches.append(NotRecalculatedBranch("p_"+options["Name"], -999, np.float32))
+      continue
+    if options["Production"] in ("ZZGG", "Lep_ZH", "Lep_WH", "ZZQQB", "ZZINDEPENDENT"): continue
+    if options["Process"] == "bkgZJets": continue
+
+    if "Options" in options:
+      options.update(dict(re.findall(r"(\w+)=([^;]+)", options["Options"])))
+
+    couplings = {}
+    if "Couplings" in options:
+      for couplingsetting in options["Couplings"].split(";"):
+        name, value = couplingsetting.split("=")
+        value = complex(value.replace(",", "+")+"j")
+        couplings[name] = value
+
+    subtractbranches = []
+    if "SubtractP" in options:
+      subtractbranches = [MELAbranchesdict[_] for _ in options["SubtractP"].split(",") if "ttH" not in _]
+
+    branches.append(
+      MELAProbability(
+        "p_"+options["Name"],
+        {
+          "J1JECNominal": MELAJECnominal1jet,
+          "J2JECNominal": MELAJECnominal2jets,
+        }[options["Cluster"]],
+        (options["Process"], options["MatrixElement"], options["Production"]),
+        couplings,
+        {
+          "JHUGen": "computeProdP",
+          "MCFM": "computeProdDecP",
+        }[options["MatrixElement"]],
+        subtractbranches,
+      )
+    )
+    MELAbranchesdict[options["Name"]] = branches[-1]
+        
+def fixjetid(infile, outfile, applyid=True, applypuid=True, folders=["ZZTree"], test_no_mela=False):
   print "Processing", infile, "-->", outfile
   with TFile(infile) as f, TFile(outfile, "CREATE", deleteifbad=True) as newf:
     for foldername in folders:
